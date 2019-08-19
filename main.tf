d4fd90ea882d416f56784455aff96c901e997678
Takayuki Kaburagi <t.kaburagi@me.com>
2019-08-19 13:45:04 +0900

    added tags

--- main.tf
+++ main.tf
@@ -12,8 +12,10 @@ resource "google_compute_instance" "vm_instance" {
 	name         = "terraform-instance"
 	machine_type = "f1-micro"
 	zone = "asia-northeast1-a"
-	tags = ["dev","k8s"]
-
+	label = map(
+		"owner", "Kabu",
+		"ttl", "100"
+	)
 	boot_disk {
 		initialize_params {
 			image = "debian-cloud/debian-9"
