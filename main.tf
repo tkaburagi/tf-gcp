b2625b6f9e79935d89e1dbc35115eb861c0d47ca
Takayuki Kaburagi <t.kaburagi@me.com>
2019-08-19 13:50:42 +0900

    added labels

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ resource "google_compute_instance" "vm_instance" {
 	name         = "terraform-instance"
 	machine_type = "f1-micro"
 	zone = "asia-northeast1-a"
-	labels = map(
-		"owner", "Kabu",
-		"ttl", "100"
-	)
+	labels = {
+		"owner" = "Kabu",
+		"ttl" = "100"
+	}
 	boot_disk {
 		initialize_params {
 			image = "debian-cloud/debian-9"
