d65fced463d7833a055acad7146a58542afeb6d8
Takayuki Kaburagi <t.kaburagi@me.com>
2019-08-19 13:59:08 +0900

    added labels

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ resource "google_compute_instance" "vm_instance" {
 	machine_type = "f1-micro"
 	zone = "asia-northeast1-a"
 	labels = {
-		"owner" = "Kabu",
-		"ttl" = "100"
+		owner = "Kabu",
+		ttl = "100"
 	}
 	boot_disk {
 		initialize_params {
