8ec72a9df4bde649a47e5e6dc6baa306b0527fbc
Takayuki Kaburagi <t.kaburagi@me.com>
2019-08-19 11:31:26 +0900

    first commit

--- main.tf
+++ main.tf
@@ -7,3 +7,21 @@ provider "google" {
 	project     = "se-kabu"
 	region      = "asia-northeast-1"
 }
+
+resource "google_compute_instance" "vm_instance" {
+	name         = "terraform-instance"
+	machine_type = "f1-micro"
+
+	boot_disk {
+		initialize_params {
+			image = "debian-cloud/debian-9"
+		}
+	}
+
+	network_interface {
+		# A default network is created for all GCP projects
+		network       = "default"
+		access_config {
+		}
+	}
+}
